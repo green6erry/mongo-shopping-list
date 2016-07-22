b7ecbd29c193897f462c08b683e62264a6468b9a
Amanda Greenberry Brown <green6erry@gmail.com>
2016-07-22 14:29:14 +0000

    thing

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://shopping:shopping@ds027425.mlab.com:27425/shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
